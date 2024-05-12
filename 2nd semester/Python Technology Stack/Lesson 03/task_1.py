6371cb03edd8296980dd3fa4116500ed043368c5
Sergey Kovalev <skovalev1@yandex.ru>
2024-05-12 19:07:04 +0300

    Create task_1.py

---
+++ 2nd semester/Python Technology Stack/Lesson 03/task_1.py
@@ -0,0 +1,7 @@
+# Request pet information from the user
+pet_type = input("Введите вид питомца: ")
+pet_age = input("Введите возраст питомца: ")
+pet_name = input("Введите кличку питомца: ")
+
+# Form and print the sentence
+print(f"Это {pet_type} по кличке \"{pet_name}\". Возраст: {pet_age} года.")
