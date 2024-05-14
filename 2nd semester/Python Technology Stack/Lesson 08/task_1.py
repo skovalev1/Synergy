9588a093b57c5a8a49b0663461d78ece5cac5ee7
Sergey Kovalev <skovalev1@yandex.ru>
2024-05-14 12:11:21 +0300

    Update task_1.py

--- 2nd semester/Python Technology Stack/Lesson 08/task_1.py
+++ 2nd semester/Python Technology Stack/Lesson 08/task_1.py
@@ -1,16 +1,25 @@
 # Вводим число N
 N = int(input())
 
-# Создаем пустой список
-numbers = []
+# Проверяем, что N удовлетворяет условию задачи
+if not 1 <= N <= 10000:
+    print("Ошибка: N должно быть в диапазоне от 1 до 10000")
+else:
+    # Создаем пустой список
+    numbers = []
 
-# Вводим N чисел и добавляем их в список
-for _ in range(N):
-    numbers.append(int(input()))
+    # Вводим N чисел и добавляем их в список
+    for _ in range(N):
+        number = int(input())
+        # Проверяем, что число удовлетворяет условию задачи
+        if abs(number) > 10**5:
+            print("Ошибка: число должно быть по модулю не больше 10^5")
+            break
+        numbers.append(number)
+    else:
+        # Переворачиваем список
+        numbers.reverse()
 
-# Переворачиваем список
-numbers.reverse()
-
-# Выводим перевернутый список
-for number in numbers:
-    print(number)
+        # Выводим перевернутый список
+        for number in numbers:
+            print(number)
