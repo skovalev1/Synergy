d280d4f905795573ee0ed3e43a4253449201f2e3
Sergey Kovalev <skovalev1@yandex.ru>
2024-05-14 11:48:37 +0300

    Update task_1.py

--- 2nd semester/Python Technology Stack/Lesson 08/task_1.py
+++ 2nd semester/Python Technology Stack/Lesson 08/task_1.py
@@ -1,1 +1,16 @@
+# Вводим число N
+N = int(input())
 
+# Создаем пустой список
+numbers = []
+
+# Вводим N чисел и добавляем их в список
+for _ in range(N):
+    numbers.append(int(input()))
+
+# Переворачиваем список
+numbers.reverse()
+
+# Выводим перевернутый список
+for number in numbers:
+    print(number)
